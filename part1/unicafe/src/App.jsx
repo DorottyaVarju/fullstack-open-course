d837ae45fccc73a9f0a027d4c7cc2bbbefd9683a
DorottyaVarju <varjudorottya@gmail.com>
2025-03-02 18:23:12 +0100

    unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,43 +1,42 @@
 import {useState} from 'react'
 
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const average = ((all !== 0) ? ((good-bad)/all) : 0)
+  const positive = ((all !== 0) ? (good/all * 100) : 0)
+
+  return(
+    <>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </>
+  )
+}
+
 const App = () => {
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
 
   const handleGoodClick = () => {
     const updatedGood = good + 1;
-    const updatedAll = updatedGood+neutral+bad;
     setGood(updatedGood);
-    setAll(updatedAll);
-    calcStats(updatedGood, bad, updatedAll);
   }
 
   const handleNeutralClick = () => {
     const updatedNeutral = neutral + 1;
-    const updatedAll = good+updatedNeutral+bad;
     setNeutral(updatedNeutral);
-    setAll(updatedAll);
-    calcStats(good, bad, updatedAll);
   }
 
   const handleBadClick = () => {
     const updatedBad = bad + 1;
-    const updatedAll = good+neutral+updatedBad;
     setBad(updatedBad);
-    setAll(updatedAll);
-    calcStats(good, updatedBad, updatedAll);
-  }
-
-  const calcStats = (g, b, all) => {
-    const resultPositivePercentage = g/all * 100;
-    const resultAverage = (g-b)/all;
-    setPositive(resultPositivePercentage);
-    setAverage(resultAverage);
   }
 
   return (
@@ -46,13 +45,7 @@ const App = () => {
       <button onClick={handleGoodClick}>good</button>
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
