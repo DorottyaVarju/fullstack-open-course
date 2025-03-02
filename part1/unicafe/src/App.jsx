f897d0d6a26fbc39e1744d04bc20a6734d5b8b5e
DorottyaVarju <varjudorottya@gmail.com>
2025-03-02 17:42:28 +0100

    unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,15 +1,44 @@
 import {useState} from 'react'
 
 const App = () => {
-  // save clicks of each button to its own state
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
-  const handleGoodClick = () => setGood(good+1);
-  const handleNeutralClick = () => setNeutral(neutral+1);
-  const handleBadClick = () => setBad(bad+1);
+  const handleGoodClick = () => {
+    const updatedGood = good + 1;
+    const updatedAll = updatedGood+neutral+bad;
+    setGood(updatedGood);
+    setAll(updatedAll);
+    calcStats(updatedGood, bad, updatedAll);
+  }
 
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1;
+    const updatedAll = good+updatedNeutral+bad;
+    setNeutral(updatedNeutral);
+    setAll(updatedAll);
+    calcStats(good, bad, updatedAll);
+  }
+
+  const handleBadClick = () => {
+    const updatedBad = bad + 1;
+    const updatedAll = good+neutral+updatedBad;
+    setBad(updatedBad);
+    setAll(updatedAll);
+    calcStats(good, updatedBad, updatedAll);
+  }
+
+  const calcStats = (g, b, all) => {
+    const resultPositivePercentage = g/all * 100;
+    const resultAverage = (g-b)/all;
+    setPositive(resultPositivePercentage);
+    setAverage(resultAverage);
+  }
 
   return (
     <div>
@@ -21,6 +50,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
